31a760e88f5eee320e7ee5e213db642b8ce09ae0
Chuy <chuymaster3@hotmail.com>
2020-11-28 19:23:34 +0900

    Initial Commit

---
+++ BuildNetflixApp/BuildNetflixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixAppApp.swift
+//  BuildNetflixApp
+//
+//  Created by CHATCHAI LOKNIYOM on 2020/11/28.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildNetflixApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildNetflixApp
+//
+//  Created by CHATCHAI LOKNIYOM on 2020/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
