38b1a881de2a99b39e59bd017f998ac9af7ebec2
Chuy <chuymaster3@hotmail.com>
2020-11-29 17:30:20 +0900

    custom tab state

--- BuildNetflixApp/MovieDetail/CustomTabSwitcher.swift
+++ BuildNetflixApp/MovieDetail/CustomTabSwitcher.swift
@@ -8,6 +8,8 @@ enum CustomTab: String, CaseIterable {
 
 struct CustomTabSwitcher: View {
     
+    @State private var currentTab: CustomTab = .episodes
+    
     var tabs: [CustomTab]
     
     func widthForTab(_ tab: CustomTab) -> CGFloat {
@@ -17,30 +19,43 @@ struct CustomTabSwitcher: View {
     var body: some View {
           VStack {
               // Custom tab picker
-              
               ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(spacing: 20) {
                     ForEach(tabs, id: \.self) { tab in
                         VStack {
                             // Red bar
                             Rectangle()
                                 .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor(currentTab == tab ? .red : .clear)
                             
                             // Button
                             Button(action: {
-                                
+                                withAnimation {
+                                    currentTab = tab
+                                }
                             }, label: {
                                 Text(tab.rawValue)
                                     .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(currentTab == tab ? .white : .gray)
                             })
                             .buttonStyle(PlainButtonStyle())
+                            .frame(width: widthForTab(tab), height: 30)
                             
                         }
                     }
                 }
               }
               
-              Text("SELECTED VIEW")
+            // Selected View
+            switch currentTab {
+            case .episodes:
+                Text("EPISODES")
+            case .trailers:
+                Text("TRAILERS")
+            case .more:
+                Text("MORE")
+            
+            }
           }
           .foregroundColor(.white)
     }
